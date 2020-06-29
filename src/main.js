a2d307d8a54c8045b7c28f9e9b550268b46f8d8e
kinggrid-yxl <675499578@qq.com>
2020-06-29 13:48:09 +0800

    1

---
+++ src/main.js
@@ -0,0 +1,67 @@
+import 'babel-polyfill'
+import 'classlist-polyfill'
+import Vue from 'vue'
+import axios from './router/axios'
+import VueAxios from 'vue-axios'
+import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import Avue from '@smallwei/avue'
+import '@smallwei/avue/lib/index.css'
+import avueFormDesign from 'pig-avue-form-design'
+import './permission' // 权限
+import './error' // 日志
+import router from './router/router'
+import store from './store'
+import { loadStyle } from './util/util'
+import * as urls from '@/config/env'
+import {
+  iconfontUrl,
+  iconfontVersion
+} from '@/config/env'
+import * as filters from './filters' // 全局filter
+import './styles/common.scss'
+import basicContainer from './components/basic-container/main'
+
+Vue.use(VueAxios, axios)
+
+Vue.use(ElementUI, {
+  size: 'medium',
+  menuType: 'text'
+})
+
+Vue.use(Avue, {
+  size: 'medium',
+  menuType: 'text'
+})
+
+Vue.use(router)
+
+Vue.use(avueFormDesign);
+
+// 注册全局容器
+Vue.component('basicContainer', basicContainer)
+
+// 加载相关url地址
+Object.keys(urls).forEach(key => {
+  Vue.prototype[key] = urls[key]
+})
+
+//加载过滤器
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
+
+// 动态加载阿里云字体库
+iconfontVersion.forEach(ele => {
+  console.log(iconfontUrl.replace('$key', ele))
+  loadStyle(iconfontUrl.replace('$key', ele))
+})
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
